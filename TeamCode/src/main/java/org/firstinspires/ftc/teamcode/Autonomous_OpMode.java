978896a597af07a72c3edf60ccca08a5eb59f8ea
Roper <griffinroboticsaz@gmail.com>
2017-12-04 14:32:43 -0700

    test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_OpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_OpMode.java
@@ -49,7 +49,7 @@ import org.firstinspires.ftc.teamcode.Movement.MovementLib;
  * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
-
+//test
 @Autonomous(name = "TestLinear", group = "Linear Autonomous")
 public class Autonomous_OpMode extends LinearCustomOpMode {
     @Override
